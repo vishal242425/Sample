1a9903ce869ec31c1d805a5cc29b0c8bb463be30
Vishal <vishalchaudhari2425@gmail.com>
2023-01-21 15:16:20 +0530

    Sample Class

---
+++ src/com/supreme/SampleClass.java
@@ -0,0 +1,13 @@
+package com.supreme;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		int a [] = {10, 20, 30, 7, 25};
+		for(int z:a) {
+			System.out.println(z);
+		}
+
+	}
+
+}
